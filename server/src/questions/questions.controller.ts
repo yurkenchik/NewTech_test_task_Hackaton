a929d435596167389826ddc4d4b53cf7d704dd0e
yurkenchik <142983974+yurkenchik@users.noreply.github.com>
2024-04-28 02:28:27 +0300

    added bussiness logic and controllers for questions

--- server/src/questions/questions.controller.ts
+++ server/src/questions/questions.controller.ts
@@ -1,4 +1,101 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, HttpException, HttpStatus, Param, Post, Req, UseGuards} from '@nestjs/common';
+import {QuestionsService} from "./questions.service";
+import {AuthUserGuard} from "../guards/auth-user.guard";
+import {AuthAssistantGuard} from "../guards/auth-assistant.guarrd";
+import { AuthMutualGuard } from 'src/guards/auth-mutual.guard';
+import {CreateQuestionDto} from "./dto/create-question.dto";
+import {Question} from "./questions.entity";
+import {ApiOperation, ApiResponse} from "@nestjs/swagger";
 
 @Controller('questions')
-export class QuestionsController {}
+export class QuestionsController {
+
+    constructor(private readonly questionsService: QuestionsService) {
+    }
+
+    @ApiOperation({
+        summary: "Getting all questions for user",
+        description: "This function returns all questions for certain user via user id in params"
+    })
+    @ApiResponse({status: 200, type: Question})
+    @UseGuards(AuthMutualGuard)
+    @Get("/get-questions")
+    async getQuestions(): Promise<Question[]> {
+        try {
+            return this.questionsService.getQuestions()
+        } catch (error) {
+            throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @ApiOperation({
+        summary: "Creation a question",
+        description: "This function creates a question that is attached for the user, that creates it via user id from token"
+    })
+    @ApiResponse({status: 200, type: Question})
+    @UseGuards(AuthUserGuard)
+    @Post("/create-question")
+    async createQuestion(@Body() createQuestionDto: CreateQuestionDto,
+                         @Req() request): Promise<Question> {
+        try {
+            const userId = request.user.id
+            return this.questionsService.createQuestion(createQuestionDto, userId)
+        } catch (error) {
+            throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @ApiOperation({
+        summary: "Getting your question",
+        description: "This function returns your question"
+    })
+    @ApiResponse({status: 200, type: Question})
+    @UseGuards(AuthUserGuard)
+    @Post("/create-question/:postId")
+    async getYourQuestion(@Req() request,
+                          @Param("postId") questionId: string): Promise<Question> {
+        try {
+            const userId = request.user.id
+            return this.questionsService.getYourQuestion(userId, questionId)
+        } catch (error) {
+            throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @ApiOperation({
+        summary: "Getting someones question",
+        description: "This function returns someones question"
+    })
+    @ApiResponse({status: 200, type: Question})
+    @UseGuards(AuthUserGuard)
+    @Post("/get-someones-question/:username/:questionId")
+    async getSomeonesQuestion(@Param("username") username: string,
+                              @Param("questionId") postId: string): Promise<Question> {
+        try {
+            return this.questionsService.getSomeonesQuestion(username, postId)
+        } catch (error) {
+            throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+
+    @ApiOperation({
+        summary: "Getting someones question",
+        description: "This function returns someones question"
+    })
+    @ApiResponse({status: 200, type: Question})
+    @UseGuards(AuthUserGuard)
+    @Post("/get-someones-question/:username/:questionId")
+    async deleteQuestion(@Req() request,
+                         @Param("questionId")  questionId: string): Promise<Question> {
+        try {
+            const userId = request.user.id
+            return this.questionsService.getSomeonesQuestion(userId, questionId)
+        } catch (error) {
+            throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+
+
+}
--- server/src/questions/questions.service.ts
+++ server/src/questions/questions.service.ts
@@ -1,4 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Question} from "./questions.entity";
+import {Repository} from "typeorm";
+import {CreateQuestionDto} from "./dto/create-question.dto";
+import {UsersService} from "../users/users.service";
 
 @Injectable()
-export class QuestionsService {}
+export class QuestionsService {
+
+    constructor(@InjectRepository(Question)
+                private readonly questionRepository: Repository<Question>,
+                private readonly usersService: UsersService) {
+    }
+
+    async getQuestions(): Promise<Question[]> {
+        return this.questionRepository.find({
+            relations: ["user"]
+        })
+    }
+
+    async getYourQuestion(userId: string, questionId: string): Promise<Question> {
+        const user = await this.usersService.getUserById(userId)
+        const question = await this.questionRepository.findOne({
+            where: {id: questionId, author: user},
+            relations: ["answers"]
+        })
+
+        return question
+    }
+
+    async getSomeonesQuestion(username: string, questionId: string): Promise<Question> {
+        const user = await this.usersService.getUserByUsername(username)
+        const question = await this.questionRepository.findOne({
+            where: {id: questionId, author: user},
+            relations: ["answers"]
+        })
+
+        return question
+    }
+
+    async createQuestion(createQuestionDto: CreateQuestionDto, userId: string): Promise<Question> {
+
+        const user = await this.usersService.getUserById(userId)
+        const question = this.questionRepository.create(createQuestionDto)
+
+        question.author = user
+
+        return await this.questionRepository.save(question)
+
+    }
+
+    async deleteQuestion(userId: string, questionId: string): Promise<Question> {
+
+        const user = await this.usersService.getUserById(userId)
+        const question = await this.questionRepository.findOne({
+            where: {id: questionId, author: user}
+        })
+
+        return await this.questionRepository.remove(question)
+
+    }
+
+}
