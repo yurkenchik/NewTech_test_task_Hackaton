e70c4d6118358cbd23cc204b1bb15499cabf5637
yurkenchik <142983974+yurkenchik@users.noreply.github.com>
2024-04-27 18:18:23 +0300

    updated tokens module

--- server/src/tokens/tokens.module.ts
+++ server/src/tokens/tokens.module.ts
@@ -2,15 +2,16 @@ import { Module } from '@nestjs/common';
 import {TokensService} from "./tokens.service";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {User} from "../users/users.entity";
+import {JwtModule} from "@nestjs/jwt";
+import {forwardRef} from "@nestjs/common/utils/forward-ref.util";
 
 @Module({
     providers: [TokensService],
     imports: [
-        TypeOrmModule.forFeature([User])
+        forwardRef(() => JwtModule)
     ],
     exports: [
         TokensService,
-        TypeOrmModule
     ]
 })
 export class TokensModule {}
