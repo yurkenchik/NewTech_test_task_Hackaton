4db50752805cdd4815fcfbc2835170a2edb2a519
sergiyclas <sergiyclas@gmail.com>
2024-04-28 06:33:20 +0300

    improved a lot of files

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -19,36 +19,39 @@ async def start_cmd(message: types.Message, session: AsyncSession):
     await message.reply_photo(media.media, caption=media.caption, reply_markup=reply_markup)
 
 
-@user_private_router.message(Command('help'))
-async def help(message: types.Message, session: AsyncSession):
+@user_private_router.callback_query(F.data.startswith('help'))
+async def help(callback: types.CallbackQuery, session: AsyncSession):
     media, reply_markup = await get_menu_content(session, level=0, menu_name='help')
 
-    await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
+    await callback.message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
 
-
+@user_private_router.callback_query(F.data.startswith('info'))
 @user_private_router.message(Command('info'))
-async def info(message: types.Message, session: AsyncSession):
+async def info(callback: types.CallbackQuery, session: AsyncSession):
     media, reply_markup = await get_menu_content(session, level=0, menu_name='info')
 
-    await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
+    await callback.message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
 
 
+@user_private_router.callback_query(F.data.startswith('emergency_call'))
 @user_private_router.message(Command('emergency_call'))
-async def emergency_call(message: types.Message, session: AsyncSession):
+async def emergency_call(callback: types.CallbackQuery, session: AsyncSession):
     media, reply_markup = await get_menu_content(session, level=0, menu_name='emergency_call')
 
-    await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
+    await callback.message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
 
 
+@user_private_router.callback_query(F.data.startswith('receive_emergencies'))
 @user_private_router.message(Command('receive_emergencies'))
-async def receive_emergencies(message: types.Message, session: AsyncSession):
+async def receive_emergencies(callback: types.CallbackQuery, session: AsyncSession):
     media, reply_markup = await get_menu_content(session, level=0, menu_name='receive_emergencies')
 
-    await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
+    await callback.message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
 
 
+@user_private_router.callback_query(F.data.startswith('support'))
 @user_private_router.message(Command('support'))
-async def support(message: types.Message, session: AsyncSession):
+async def support(callback: types.CallbackQuery, session: AsyncSession):
     media, reply_markup = await get_menu_content(session, level=0, menu_name='support')
 
-    await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
+    await callback.message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
--- keyboards/inline.py
+++ keyboards/inline.py
@@ -3,12 +3,12 @@ from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-class MenuCallBack(CallbackData, prefix='menu'):
-    level: int
-    menu_name: str
-    category: int | None = None
-    page: int = 1
-    command_id: int | None = None
+# class MenuCallBack(CallbackData, prefix='/'):
+#     level: int
+#     menu_name: str
+#     category: int | None = None
+#     page: int = 1
+#     command_id: int | None = None
 
 
 def get_user_main_btns(*, level: int, sizes: tuple[int] = (1,)):
@@ -20,9 +20,33 @@ def get_user_main_btns(*, level: int, sizes: tuple[int] = (1,)):
         'Отримувати сповіщення термінових запитів': 'receive_emergencies',
         'Підтримка': 'support',
     }
-
-    for text, menu_name in btns.items():
-        keyboard.add(InlineKeyboardButton(text=text,
-                                          callback_data=MenuCallBack(level=level, menu_name=menu_name).pack()))
+    for text, data in btns.items():
+        keyboard.add(InlineKeyboardButton(text=text, callback_data=data))
 
     return keyboard.adjust(*sizes).as_markup()
+
+
+# def get_user_main_btns(*, level: int, sizes: tuple[int] = (1,)):
+#     keyboard = InlineKeyboardBuilder()
+#     btns = {
+#         'Інструкція до бота': 'help',
+#         'Інформація про сайт і бота': 'info',
+#         'Кинути терміновий запит': 'emergency_call',
+#         'Отримувати сповіщення термінових запитів': 'receive_emergencies',
+#         'Підтримка': 'support',
+#     }
+#
+#     for text, menu_name in btns.items():
+#         keyboard.add(InlineKeyboardButton(text=text,
+#                                           callback_data=MenuCallBack(level=level, menu_name=menu_name).pack()))
+#
+#     return keyboard.adjust(*sizes).as_markup()
+
+
+# def get_callback_btns(*, btns: dict[str, str], sizes: tuple[int] = (2,)):
+#     keyboard = InlineKeyboardBuilder()
+#
+#     for text, data in btns.items():
+#         keyboard.add(InlineKeyboardButton(text=text, callback_data=data))
+#
+#     return keyboard.adjust(*sizes).as_markup()
